27b2af48166212cb50fdd3804fab1a42baafa3d1
shoba-p <shobapallapati@gmail.com>
2026-07-11 21:08:54 +0530

    DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,24 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        byte age = 19;
+        short year = 2026;
+        int marks = 850;
+        long population = 1400000000L;
+
+        float percentage = 81.9f;
+        double cgpa = 7.09;
+
+        char grade = 'A';
+        boolean passed = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Year: " + year);
+        System.out.println("Marks: " + marks);
+        System.out.println("Population: " + population);
+        System.out.println("Percentage: " + percentage);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + passed);
+    }
+}
