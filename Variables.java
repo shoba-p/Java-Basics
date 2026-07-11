95dc4fc6b14a5cae5574e7489feac70faafcdb1f
shoba-p <shobapallapati@gmail.com>
2026-07-11 21:06:26 +0530

    Variables.java

---
+++ Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 19;
+        double cgpa = 7.09;
+        char grade = 'A';
+        boolean student = true;
+        String name = "Pallapati Shoba Rani";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + student);
+    }
+}
