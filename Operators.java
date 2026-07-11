83d81ebd1c3be070f217b5ad275df3ed651668f3
shoba-p <shobapallapati@gmail.com>
2026-07-11 21:10:35 +0530

    Add Operators.java

---
+++ Operators.java
@@ -0,0 +1,12 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+    }
+}
